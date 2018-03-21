7e798f110347bf03ff351c4bb3cbede8512021e9
Connie <itsmeconniec@gmail.com>
2018-03-21 20:39:05 +0000

    My first app

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app  = Flask("MyApp")
 
@@ -13,5 +13,15 @@ def idontexist():
 @app.route("/connie")
 def connie():
     return "My favourite dessert is ice cream!"
+
+@app.route("/<name>")
+def hello_someone(name):
+    return render_template("hello.html", name=name.title())
+
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    form_data = request.form
+    print form_data["email"]
+    return "All OK - thanks so much, dude!"
 
 app.run(debug=True)
